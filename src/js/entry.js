72027663874370f75600a6be3a512fac75ecf657
Rikki Gibson <rikkigibson@gmail.com>
2015-11-21 13:14:51 -0800

    Style browse page and navbar

--- src/js/entry.js
+++ src/js/entry.js
@@ -7,12 +7,21 @@ import { Router, Route, Link } from 'react-router';
 
 class Navbar extends React.Component {
 	render() {
+    var pathname = this.props.location.pathname;
 		return (
-			<div>
-				<div><Link to="/favorites">Favorites</Link></div>
-				<div><Link to="/map">Map</Link></div>
+			<ul className="nav">
+				<li className="nav" id="favorites-link">
+          <Link className={pathname.includes("favorites") ? "nav active" : "nav"} to="favorites">
+            Favorites
+          </Link>
+        </li>
+				<li className="nav" id="browse-link">
+          <Link className={pathname.includes("browse") ? "nav active" : "nav"} to="browse">
+            Browse
+          </Link>
+        </li>
 				{this.props.children}
-			</div>
+			</ul>
 		);
 	}
 }
@@ -21,7 +30,7 @@ ReactDOM.render(
 	<Router>
 		<Route path="/" component={Navbar}>
 			<Route path="favorites" component={FavoritesTable}></Route>
-			<Route path="map" component={TransitBrowse}></Route>
+			<Route path="browse" component={TransitBrowse}></Route>
 		</Route>
 	</Router>,
 	document.getElementById('main'));
